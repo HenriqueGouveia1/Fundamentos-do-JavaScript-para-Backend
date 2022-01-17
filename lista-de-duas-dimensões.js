1c0bd52738fb547dc76db50ce569d04ff9fcb828
HenriqueGouveia1 <henriquegs002@gmail.com>
2022-01-17 18:57:20 -0300

    Lista de duas dimensoes, interessante.

---
+++ lista-de-duas-dimensões.js
@@ -0,0 +1,6 @@
+const alunos= ['João','Libni','Fabo']
+const medias= [8,10,9]
+
+let relacaoNotaluno = [alunos, medias]
+
+console.log(`O aluno ${relacaoNotaluno[0][0]},tem a média: ${relacaoNotaluno[1][0]}`)
